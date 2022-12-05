bcd3bac80a35fdd0310bd7a857f487de604b4170
Alex <alexiv.04@mail.ru>
2022-12-05 18:57:04 +0700

    service commit

--- app/src/main/java/com/example/ivanov_pr251/MainActivity.java
+++ app/src/main/java/com/example/ivanov_pr251/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.ivanov_pr251;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
+    }
+
+    public void startNewActivity (View v){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
